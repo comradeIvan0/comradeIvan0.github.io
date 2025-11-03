843f6ee45467ed07530783c1018c9d586adb0826
comradeIvan0 <comradeivano@gmail.com>
2025-11-02 20:28:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const shareButtons = document.querySelectorAll('.tile-share-button')
+const shareButtons = document.querySelectorAll('.tile-share-button');
 console.log(shareButtons)
 
 async function copyText(e) {
@@ -15,4 +15,23 @@ async function copyText(e) {
 }
 
 shareButtons.forEach(shareButton =>
+    shareButton.addEventListener('click', copyText))
+
+const shareButtons1 = document.querySelectorAll('.share-button');
+console.log(shareButtons1)
+
+async function copyText(e) {
+//prevent button going to site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+shareButtons1.forEach(shareButton =>
     shareButton.addEventListener('click', copyText))
